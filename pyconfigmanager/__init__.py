108c247e20bdb3e86a053dd9e433ca040b8a9e68
miacro <fqguozhou@gmail.com>
2018-08-10 15:45:35 +0800

    ...

--- pyconfigmanager/__init__.py
+++ pyconfigmanager/__init__.py
@@ -2,21 +2,21 @@ from pyconfigmanager import utils
 from .config import Config
 
 
-def get_config(schema=[], values=[], **kwargs):
+def getconfig(schema=[], values=[], **kwargs):
     if (not isinstance(schema, list)) and (not isinstance(schema, tuple)):
         schema = [schema]
     if (not isinstance(values, list)) and (not isinstance(values, tuple)):
         values = [values]
     config = Config()
-    for item in normalize_dict(schema):
+    for item in normalizedict(schema):
         config.update_schema(
             schema=utils.get_item_by_category(item, **kwargs), merge=True)
-    for item in normalize_dict(values):
+    for item in normalizedict(values):
         config.update_values(values=utils.get_item_by_category(item, **kwargs))
     return config
 
 
-def normalize_dict(data):
+def normalizedict(data):
     for item in data:
         if isinstance(item, str):
             for _, entry in enumerate(utils.load_config(item)):
--- pyconfigmanager/test/test_init.py
+++ pyconfigmanager/test/test_init.py
@@ -1,30 +1,30 @@
 import unittest
-from pyconfigmanager import get_config
+from pyconfigmanager import getconfig
 import os
 
 
 class TestGlobal(unittest.TestCase):
-    def test_get_config(self):
+    def test_getconfig(self):
         filedir = os.path.join(
             os.path.dirname(os.path.abspath(__file__)), "files")
         schemafile = os.path.join(filedir, "schema.yaml")
         valuesfile = os.path.join(filedir, "values.json")
         schema = {"test": {"a": 12, "b": {"c": 23, "d": "hello"}}}
         values = {"test": {"a": "hello", "b": 56, "c": {"d": 67, "e": "hell"}}}
-        config = get_config(schema=schemafile, category="test")
+        config = getconfig(schema=schemafile, category="test")
         self.assertEqual(config.a, None)
         self.assertEqual(config.b, 12)
         self.assertEqual(config.c.d, 12)
         self.assertEqual(config.c.e, "hello")
 
-        config = get_config(
+        config = getconfig(
             schema=schemafile, values=valuesfile, category="test")
         self.assertEqual(config.a, "text")
         self.assertEqual(config.b, 90)
         self.assertEqual(config.c.d, 76)
         self.assertEqual(config.c.e, "he")
 
-        config = get_config(
+        config = getconfig(
             schema=[schema, schemafile],
             values=[valuesfile, values],
             category="test")
